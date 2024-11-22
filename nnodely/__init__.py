c12b80f5f353bc91f2b1438dfde78bd01ac8c224
tonegas <tonegas@gmail.com>
2024-11-22 17:05:12 +0100

    Added the loger and utils

--- nnodely/__init__.py
+++ nnodely/__init__.py
@@ -4,6 +4,9 @@ __version__ = importlib.metadata.version(__package__)
 import sys
 major, minor = sys.version_info.major, sys.version_info.minor
 
+import logging
+LOG_LEVEL = logging.INFO
+
 if major < 3:
     sys.exit("Sorry, Python 2 is not supported. You need Python >= 3.6 for "+__package__+".")
 elif minor < 10:
