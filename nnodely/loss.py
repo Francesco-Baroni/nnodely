5e98e1b7ab45bf57e6436ba90f66fd28607e5cb9
tonegas <tonegas@gmail.com>
2024-11-24 13:34:54 +0100

    Fix on file loss

--- nnodely/loss.py
+++ nnodely/loss.py
@@ -4,18 +4,16 @@ from nnodely.utils import check
 
 available_losses = ['mse', 'rmse', 'mae']
 
-'''
-class CustomRMSE(nn.Module):
-    def __init__(self):
-        super(CustomRMSE, self).__init__()
-        self.mse = nn.MSELoss()
+# class CustomRMSE(nn.Module):
+#     def __init__(self):
+#         super(CustomRMSE, self).__init__()
+#         self.mse = nn.MSELoss()
+#
+#     def forward(self, inA, inB):
+#         #assert predictions.keys() == labels.keys(), "Keys of predictions and labels must match"
+#         #loss = torch.sqrt(self.mse(inA, inB))
+#         return self.mse(inA, inB)
 
-    def forward(self, inA, inB):
-        #assert predictions.keys() == labels.keys(), "Keys of predictions and labels must match"
-        #loss = torch.sqrt(self.mse(inA, inB))
-        return self.mse(inA, inB)
-'''
-    
 class CustomLoss(nn.Module):
     def __init__(self, loss_type='mse'):
         super(CustomLoss, self).__init__()
