56285fa145ee1f183c1e09a03bbbfe5c45d392d7
Giovanni Scialla <giovanni_scialla1999@outlook.it>
2025-02-06 16:24:18 +0100

    Equation Learner block - draft

--- examples/example_equationlearner.py
+++ examples/example_equationlearner.py
@@ -9,20 +9,51 @@ x = Input('x')
 F = Input('F')
 
 print("------------------------EXAMPLE 1------------------------")
-def func1(K1):
-    return torch.sin(K1)
+## Create an equation learner block with pure nnodely functions
+#W = Parameter(name='W', dimensions=[1,3], values=[[[1.0,1.0,1.0]]])
+linear_layer = Linear(output_dimension=3, W_init=init_constant, W_init_params={'value':0}, b_init=init_constant, b_init_params={'value':0}, b=False)(x.last())
+cos_layer = Cos(Select(linear_layer,0))
+sin_layer = Sin(Select(linear_layer,1))
+tan_layer = Tan(Select(linear_layer,2))
+concatenate_layer = Concatenate(Concatenate(tan_layer,sin_layer),cos_layer)
 
-def func2(K2):
-    return torch.cos(K2)
+#equation_learner = EquationLearner(functions=[Sin, Cos, Tan])
+#out = Output('out',equation_learner(x.last()))
+out_linear = Output('out_linear',linear_layer)
+out_cos = Output('out_cos',cos_layer)
+out_sin = Output('out_sin',sin_layer)
+out_tan = Output('out_tan',tan_layer)
+out_concatenate = Output('out_concatenate',concatenate_layer)
+example = Modely(visualizer=MPLVisualizer())
+#example.addModel('eqlearner',out)
+example.addModel('model',[out_linear,out_cos,out_sin,out_tan,out_concatenate])
+example.neuralizeModel()
+print(example({'x':[1]}))
 
-parfun1 = ParamFun(func1)
-parfun2 = ParamFun(func2)
-equation_learner = EquationLearner([func1, func2])
-
+print("------------------------EXAMPLE 2------------------------")
+## Create an equation learner block similar to Example 1 but using EquationLearner function
+equation_learner = EquationLearner(functions=[Tan, Sin, Cos])
 out = Output('out',equation_learner(x.last()))
 example = Modely(visualizer=MPLVisualizer())
-example.addModel('eqlearner',out)
+example.addModel('model',[out])
 example.neuralizeModel()
-print(example({'x':[1],'F':[1]}))
-print(example({'x':[1,2],'F':[1,2]}))
-example.visualizer.showFunctions(list(example.model_def['Functions'].keys()),xlim=[[-5,5],[-1,1]])
+print(example({'x':[1]}))
+
+# print("------------------------EXAMPLE 3------------------------")
+# ## Create an equation learner block with parametric functions
+# def func1(K1):
+#     return torch.sin(K1)
+
+# def func2(K2):
+#     return torch.cos(K2)
+
+# parfun1 = ParamFun(func1)
+# parfun2 = ParamFun(func2)
+# equation_learner = EquationLearner([func1, func2])
+
+# out = Output('out',equation_learner(x.last()))
+# example = Modely(visualizer=MPLVisualizer())
+# example.addModel('eqlearner',out)
+# example.neuralizeModel()
+# print(example({'x':[1],'F':[1]}))
+# print(example({'x':[1,2],'F':[1,2]}))
--- nnodely/equationlearner.py
+++ nnodely/equationlearner.py
@@ -10,6 +10,9 @@ from nnodely.model import Model
 from nnodely.parameter import Parameter, Constant
 from nnodely.utils import check, merge, enforce_types
 
+from nnodely.linear import Linear
+from nnodely.part import Select
+from nnodely.arithmetic import Concatenate
 
 equationlearner_relation_name = 'EquationLearner'
 
@@ -21,106 +24,131 @@ class EquationLearner(NeuObj):
 
         # input parameters
         self.functions = functions
+        self.n_activations = len(self.functions)
+        check(self.n_activations > 0, ValueError, 'At least one activation function must be provided')
         super().__init__(equationlearner_relation_name + str(NeuObj.count))
         for func in self.functions:
-            code = textwrap.dedent(inspect.getsource(func)).replace('\"', '\'')
-            self.json['Functions'][self.name] = {
-                'code' : code,
-                'name' : func.__name__,
-            }
-        self.json['Functions'][self.name]['params_and_consts'] = []
+            check(callable(func), TypeError, 'The activation functions must be callable')
 
-    def __call__(self, *obj):
-        stream_name = equationlearner_relation_name + str(Stream.count)
-
-        for func in self.functions:
-            funinfo = inspect.getfullargspec(func)
-            n_function_input = len(funinfo.args)
-            n_call_input = len(obj)
-            n_new_constants_and_params = n_function_input - n_call_input
-
-            if 'n_input' not in self.json['Functions'][self.name]:
-                self.json['Functions'][self.name]['n_input'] = n_call_input
-                self.__set_params_and_consts(n_new_constants_and_params)
-
-                input_dimensions = []
-                input_types = []
-                for ind, o in enumerate(obj):
-                    if type(o) in (int,float,list):
-                        obj_type = Constant
-                    else:
-                        obj_type = type(o)
-                    o = toStream(o)
-                    check(type(o) is Stream, TypeError,
-                        f"The type of {o} is {type(o)} and is not supported for ParamFun operation.")
-                    input_types.append(obj_type)
-                    input_dimensions.append(o.dim)
-
-                self.json['Functions'][self.name]['in_dim'] = copy.deepcopy(input_dimensions)
-                self.__infer_output_dimensions(input_types, input_dimensions)
-                self.json['Functions'][self.name]['out_dim'] = copy.deepcopy(self.output_dimension)
-
-            # Create the missing parameters
-            missing_params = n_new_constants_and_params - len(self.json['Functions'][self.name]['params_and_consts'])
-            check(missing_params == 0, ValueError, f"The function is called with different number of inputs.")
-
-            stream_json = copy.deepcopy(self.json)
-            input_names = []
-            for ind, o in enumerate(obj):
-                o = toStream(o)
-                check(type(o) is Stream, TypeError,
-                    f"The type of {o} is {type(o)} and is not supported for ParamFun operation.")
-                stream_json = merge(stream_json, o.json)
-                input_names.append(o.name)
-
-        output_dimension = copy.deepcopy(self.output_dimension)
-        stream_json['Relations'][stream_name] = [equationlearner_relation_name, input_names, self.name]
-
-        return Stream(stream_name, stream_json, output_dimension)
-
-    def __set_params_and_consts(self, n_new_constants_and_params):
-        pass
+    def __call__(self, inputs):
+        linear_layer = Linear(output_dimension=self.n_activations)(inputs)
+        for idx, func in enumerate(self.functions):
+            if idx == 0:
+                out = func(Select(linear_layer,idx))
+            else:
+                out = Concatenate(out, func(Select(linear_layer,idx)))
+        return out
 
 
-class EquationLearner_Layer(nn.Module):
-    def __init__(self, functions, input_dim, output_dim, hidden_layers):
-        super(EquationLearner_Layer, self).__init__()
-        self.hidden_layers = nn.ModuleList()
+# class EquationLearner(NeuObj):
+#     @enforce_types
+#     def __init__(self, functions:list|dict|None = None) -> Stream:
 
-        # Define activation functions: I (identity), sin, cos, sigma, and sech
-        self.activations = {
-            'identity': lambda x: x,
-            'sin': torch.sin,
-            'cos': torch.cos,
-            'sigma': lambda x: 1 / (1 + torch.exp(-x)),
-            'sech': lambda x: 2 / (torch.exp(x) + torch.exp(-x))
-        }
+#         self.relation_name = equationlearner_relation_name
 
-        # Input layer
-        current_dim = input_dim
-        for _ in range(hidden_layers):
-            layer = nn.Linear(current_dim, len(functions) * current_dim)
-            self.hidden_layers.append(layer)
-            current_dim = len(functions) * current_dim
+#         # input parameters
+#         self.functions = functions
+#         super().__init__(equationlearner_relation_name + str(NeuObj.count))
+#         for func in self.functions:
+#             check(callable(func), TypeError, 'The activation functions must be callable')
+#             code = textwrap.dedent(inspect.getsource(func)).replace('\"', '\'')
+#             self.json['Functions'][self.name] = {
+#                 'code' : code,
+#                 'name' : func.__name__,
+#             }
+#         self.json['Functions'][self.name]['params_and_consts'] = []
 
-        # Output layer
-        self.output_layer = nn.Linear(current_dim, output_dim)
+#     def __call__(self, *obj):
+#         stream_name = equationlearner_relation_name + str(Stream.count)
 
-    def forward(self, x):
-        for layer in self.hidden_layers:
-            x = layer(x)
-            # Apply all activation functions
-            activation_results = torch.cat([
-                self.activations["identity"](x),
-                self.activations["sin"](x),
-                self.activations["cos"](x),
-                self.activations["sigma"](x),
-                self.activations["sech"](x)
-            ], dim=-1)
-            x = activation_results
-        return self.output_layer(x)
+#         for func in self.functions:
+#             funinfo = inspect.getfullargspec(func)
+#             n_function_input = len(funinfo.args)
+#             n_call_input = len(obj)
+#             n_new_constants_and_params = n_function_input - n_call_input
 
-def createEquationLearner(self, *func_params):
-    return EquationLearner_Layer(func=func_params[0])
+#             if 'n_input' not in self.json['Functions'][self.name]:
+#                 self.json['Functions'][self.name]['n_input'] = n_call_input
+#                 self.__set_params_and_consts(n_new_constants_and_params)
 
-setattr(Model, equationlearner_relation_name, createEquationLearner)
+#                 input_dimensions = []
+#                 input_types = []
+#                 for ind, o in enumerate(obj):
+#                     if type(o) in (int,float,list):
+#                         obj_type = Constant
+#                     else:
+#                         obj_type = type(o)
+#                     o = toStream(o)
+#                     check(type(o) is Stream, TypeError,
+#                         f"The type of {o} is {type(o)} and is not supported for ParamFun operation.")
+#                     input_types.append(obj_type)
+#                     input_dimensions.append(o.dim)
+
+#                 self.json['Functions'][self.name]['in_dim'] = copy.deepcopy(input_dimensions)
+#                 self.__infer_output_dimensions(input_types, input_dimensions)
+#                 self.json['Functions'][self.name]['out_dim'] = copy.deepcopy(self.output_dimension)
+
+#             # Create the missing parameters
+#             missing_params = n_new_constants_and_params - len(self.json['Functions'][self.name]['params_and_consts'])
+#             check(missing_params == 0, ValueError, f"The function is called with different number of inputs.")
+
+#             stream_json = copy.deepcopy(self.json)
+#             input_names = []
+#             for ind, o in enumerate(obj):
+#                 o = toStream(o)
+#                 check(type(o) is Stream, TypeError,
+#                     f"The type of {o} is {type(o)} and is not supported for ParamFun operation.")
+#                 stream_json = merge(stream_json, o.json)
+#                 input_names.append(o.name)
+
+#         output_dimension = copy.deepcopy(self.output_dimension)
+#         stream_json['Relations'][stream_name] = [equationlearner_relation_name, input_names, self.name]
+
+#         return Stream(stream_name, stream_json, output_dimension)
+
+#     def __set_params_and_consts(self, n_new_constants_and_params):
+#         pass
+
+
+# class EquationLearner_Layer(nn.Module):
+#     def __init__(self, functions, input_dim, output_dim, hidden_layers):
+#         super(EquationLearner_Layer, self).__init__()
+#         self.hidden_layers = nn.ModuleList()
+
+#         # Define activation functions: I (identity), sin, cos, sigma, and sech
+#         self.activations = {
+#             'identity': lambda x: x,
+#             'sin': torch.sin,
+#             'cos': torch.cos,
+#             'sigma': lambda x: 1 / (1 + torch.exp(-x)),
+#             'sech': lambda x: 2 / (torch.exp(x) + torch.exp(-x))
+#         }
+
+#         # Input layer
+#         current_dim = input_dim
+#         for _ in range(hidden_layers):
+#             layer = nn.Linear(current_dim, len(functions) * current_dim)
+#             self.hidden_layers.append(layer)
+#             current_dim = len(functions) * current_dim
+
+#         # Output layer
+#         self.output_layer = nn.Linear(current_dim, output_dim)
+
+#     def forward(self, x):
+#         for layer in self.hidden_layers:
+#             x = layer(x)
+#             # Apply all activation functions
+#             activation_results = torch.cat([
+#                 self.activations["identity"](x),
+#                 self.activations["sin"](x),
+#                 self.activations["cos"](x),
+#                 self.activations["sigma"](x),
+#                 self.activations["sech"](x)
+#             ], dim=-1)
+#             x = activation_results
+#         return self.output_layer(x)
+
+# def createEquationLearner(self, *func_params):
+#     return EquationLearner_Layer(func=func_params[0])
+
+# setattr(Model, equationlearner_relation_name, createEquationLearner)
