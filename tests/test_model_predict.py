45358e530e4c674c73e653d7dae42d7148e04351
tonegas <tonegas@gmail.com>
2025-02-23 18:18:32 +0100

    Added a long test for LocalModel

--- tests/test_model_predict.py
+++ tests/test_model_predict.py
@@ -1530,6 +1530,92 @@ class ModelyPredictTest(unittest.TestCase):
                                  [14.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
                                  [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]],  results['out1'])
 
+    def test_localmodel(self):
+
+        x = Input('x')
+        F = Input('F')
+        activationA = Fuzzify(2, [0, 1], functions='Triangular')(x.tw(1))
+        activationB = Fuzzify(2, [0, 1], functions='Triangular')(F.tw(1))
+
+        def myFun(in1, p1, p2):
+            return p1 * in1 + p2
+
+        p1_0 = Parameter('p1_0', values=[[1]])
+        p1_1 = Parameter('p1_1', values=[[2]])
+        p2_0 = Parameter('p2_0', values=[[2]])
+        p2_1 = Parameter('p2_1', values=[[3]])
+
+        def input_function_gen(idx_list):
+            if idx_list == [0, 0]:
+                p1, p2 = p1_0, p2_0
+            if idx_list == [0, 1]:
+                p1, p2 = p1_0, p2_1
+            if idx_list == [1, 0]:
+                p1, p2 = p1_1, p2_0
+            if idx_list == [1, 1]:
+                p1, p2 = p1_1, p2_1
+            return ParamFun(myFun, parameters=[p1, p2])
+
+        def output_function_gen(idx_list):
+            pfir = Parameter('pfir_' + str(idx_list), tw=1, dimensions=2,
+                             values=[[1 + idx_list[0], 2 + idx_list[1]], [3 + idx_list[0], 4 + idx_list[1]]])
+            return Fir(2, parameter=pfir)
+
+        loc = LocalModel(input_function=input_function_gen, output_function=output_function_gen, pass_indexes=True)(x.tw(1), (activationA, activationB))
+        # Example of the structure of the local model
+        pfir00 = Parameter('N_pfir_[0, 0]', tw=1, dimensions=2, values=[[1, 2], [3, 4]])
+        pfir01 = Parameter('N_pfir_[0, 1]', tw=1, dimensions=2, values=[[1, 3], [3, 5]])
+        pfir10 = Parameter('N_pfir_[1, 0]', tw=1, dimensions=2, values=[[2, 2], [4, 4]])
+        pfir11 = Parameter('N_pfir_[1, 1]', tw=1, dimensions=2, values=[[2, 3], [4, 5]])
+        parfun_00 = ParamFun(myFun, parameters=[p1_0, p2_0])(x.tw(1))
+        parfun_01 = ParamFun(myFun, parameters=[p1_0, p2_1])(x.tw(1))
+        parfun_10 = ParamFun(myFun, parameters=[p1_1, p2_0])(x.tw(1))
+        parfun_11 = ParamFun(myFun, parameters=[p1_1, p2_1])(x.tw(1))
+        out_in_00 = Output('parfun00', parfun_00)
+        out_in_01 = Output('parfun01', parfun_01)
+        out_in_10 = Output('parfun10', parfun_10)
+        out_in_11 = Output('parfun11', parfun_11)
+        actA = Output('fuzzyA', activationA)
+        actB = Output('fuzzyB', activationB)
+        act_selA0 = Select(activationA, 0)
+        act_selA1 = Select(activationA, 1)
+        act_selB0 = Select(activationB, 0)
+        act_selB1 = Select(activationB, 1)
+        out_act_selA0 = Output('fuzzy_selA0', act_selA0)
+        out_act_selA1 = Output('fuzzy_selA1', act_selA1)
+        out_act_selB0 = Output('fuzzy_selB0', act_selB0)
+        out_act_selB1 = Output('fuzzy_selB1', act_selB1)
+        mul00 = parfun_00 * act_selA0 * act_selB0
+        mul01 = parfun_01 * act_selA0 * act_selB1
+        mul10 = parfun_10 * act_selA1 * act_selB0
+        mul11 = parfun_11 * act_selA1 * act_selB1
+        out_mul00 = Output('mul00', mul00)
+        out_mul01 = Output('mul01', mul01)
+        out_mul10 = Output('mul10', mul10)
+        out_mul11 = Output('mul11', mul11)
+        fir00 = Fir(2, parameter=pfir00)(mul00)
+        fir01 = Fir(2, parameter=pfir01)(mul01)
+        fir10 = Fir(2, parameter=pfir10)(mul10)
+        fir11 = Fir(2, parameter=pfir11)(mul11)
+        out_fir00 = Output('fir00', fir00)
+        out_fir01 = Output('fir01', fir01)
+        out_fir10 = Output('fir10', fir10)
+        out_fir11 = Output('fir11', fir11)
+        sum = fir00 + fir01 + fir10 + fir11
+        out_sum = Output('out_sum', sum)
+        out = Output('out', loc)
+        test = Modely(visualizer=None)
+        test.addModel('all_out', [out_in_00, out_in_01, out_in_10, out_in_11,
+                                     out_act_selA0, out_act_selA1, out_act_selB0, out_act_selB1,
+                                     out_mul00, out_mul01, out_mul10, out_mul11,
+                                     out_fir00, out_fir01, out_fir10, out_fir11,
+                                     out_sum])
+        test.addModel('out', out)
+        test.neuralizeModel(0.5)
+        # Three semples with a dimensions 2
+        result = test({'x': [0, 1, -2, 3], 'F': [-2, 2, 1, 5]})
+        self.assertEqual(result['out_sum'],result['out'])
+
     def test_integrate_derivate(self):
         input = Input('in1')
 
