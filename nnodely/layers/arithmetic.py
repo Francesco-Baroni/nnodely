5cc8509c01f4eb7604950d6098036536cdc47aa7
Francesco-Baroni <francesco.baroni-1@studenti.unitn.it>
2025-05-08 17:40:12 +0200

    Added sign function and moved arithmetic_cheks in utils

--- nnodely/layers/arithmetic.py
+++ nnodely/layers/arithmetic.py
@@ -5,6 +5,7 @@ from nnodely.basic.relation import ToStream, Stream, toStream
 from nnodely.basic.model import Model
 from nnodely.support.utils import check, merge, enforce_types, get_window
 from nnodely.layers.parameter import Parameter, Constant
+from nnodely.support.utils import binary_cheks
 
 # Binary operators
 add_relation_name = 'Add'
@@ -15,28 +16,10 @@ pow_relation_name = 'Pow'
 
 # Unary operators
 neg_relation_name = 'Neg'
+sign_relation_name = 'Sign'
 
 # Merge operator
 sum_relation_name = 'Sum'
-
-def aritmetic_cheks(self, obj1, obj2, name):
-    obj1,obj2 = toStream(obj1),toStream(obj2)
-    check(type(obj1) is Stream,TypeError,
-          f"The type of {obj1} is {type(obj1)} and is not supported for add operation.")
-    check(type(obj2) is Stream,TypeError,
-          f"The type of {obj2} is {type(obj2)} and is not supported for add operation.")
-    window_obj1 = get_window(obj1)
-    window_obj2 = get_window(obj2)
-    if window_obj1 is not None and window_obj2 is not None:
-        check(window_obj1==window_obj2, TypeError,
-              f"For {name} the time window type must match or None but they were {window_obj1} and {window_obj2}.")
-        check(obj1.dim[window_obj1] == obj2.dim[window_obj2], ValueError,
-              f"For {name} the time window must match or None but they were {window_obj1}={obj1.dim[window_obj1]} and {window_obj2}={obj2.dim[window_obj2]}.")
-    check(obj1.dim['dim'] == obj2.dim['dim'] or obj1.dim == {'dim':1} or obj2.dim == {'dim':1}, ValueError,
-          f"For {name} the dimension of {obj1.name} = {obj1.dim} must be the same of {obj2.name} = {obj2.dim}.")
-    dim = obj1.dim | obj2.dim
-    dim['dim'] = max(obj1.dim['dim'], obj2.dim['dim'])
-    return obj1, obj2, dim
 
 class Add(Stream, ToStream):
     """
@@ -59,7 +42,7 @@ class Add(Stream, ToStream):
     """
     @enforce_types
     def __init__(self, obj1:Stream|Parameter|Constant|int|float, obj2:Stream|Parameter|Constant|int|float) -> Stream:
-        obj1, obj2, dim = aritmetic_cheks(self, obj1, obj2, 'addition operators (+)')
+        obj1, obj2, dim = binary_cheks(self, obj1, obj2, 'addition operators (+)')
         super().__init__(add_relation_name + str(Stream.count),merge(obj1.json,obj2.json),dim)
         self.json['Relations'][self.name] = [add_relation_name,[obj1.name,obj2.name]]
 
@@ -81,7 +64,7 @@ class Sub(Stream, ToStream):
     """
     @enforce_types
     def __init__(self, obj1:Stream|Parameter|Constant|int|float, obj2:Stream|Parameter|Constant|int|float) -> Stream:
-        obj1, obj2, dim = aritmetic_cheks(self, obj1, obj2, 'subtraction operators (-)')
+        obj1, obj2, dim = binary_cheks(self, obj1, obj2, 'subtraction operators (-)')
         super().__init__(sub_relation_name + str(Stream.count),merge(obj1.json,obj2.json),dim)
         self.json['Relations'][self.name] = [sub_relation_name,[obj1.name,obj2.name]]
 
@@ -102,7 +85,7 @@ class Mul(Stream, ToStream):
     """
     @enforce_types
     def __init__(self, obj1:Stream|Parameter|Constant|int|float, obj2:Stream|Parameter|Constant|int|float) -> Stream:
-        obj1, obj2, dim = aritmetic_cheks(self, obj1, obj2, 'multiplication operators (*)')
+        obj1, obj2, dim = binary_cheks(self, obj1, obj2, 'multiplication operators (*)')
         super().__init__(mul_relation_name + str(Stream.count),merge(obj1.json,obj2.json),dim)
         self.json['Relations'][self.name] = [mul_relation_name,[obj1.name,obj2.name]]
 
@@ -123,7 +106,7 @@ class Div(Stream, ToStream):
     """
     @enforce_types
     def __init__(self, obj1:Stream|Parameter|Constant|int|float, obj2:Stream|Parameter|Constant|int|float) -> Stream:
-        obj1, obj2, dim = aritmetic_cheks(self, obj1, obj2, 'division operators (/) ')
+        obj1, obj2, dim = binary_cheks(self, obj1, obj2, 'division operators (/) ')
         super().__init__(div_relation_name + str(Stream.count),merge(obj1.json,obj2.json),dim)
         self.json['Relations'][self.name] = [div_relation_name,[obj1.name,obj2.name]]
 
@@ -148,7 +131,7 @@ class Pow(Stream, ToStream):
     """
     @enforce_types
     def __init__(self, obj1:Stream|Parameter|Constant|int|float, obj2:Stream|Parameter|Constant|int|float) -> Stream:
-        obj1, obj2, dim = aritmetic_cheks(self, obj1, obj2, 'pow operators (**)')
+        obj1, obj2, dim = binary_cheks(self, obj1, obj2, 'pow operators (**)')
         super().__init__(pow_relation_name + str(Stream.count),merge(obj1.json,obj2.json),dim)
         self.json['Relations'][self.name] = [pow_relation_name,[obj1.name,obj2.name]]
 
@@ -169,6 +152,24 @@ class Neg(Stream, ToStream):
               f"The type of {obj} is {type(obj)} and is not supported for neg operation.")
         super().__init__(neg_relation_name+str(Stream.count), obj.json, obj.dim)
         self.json['Relations'][self.name] = [neg_relation_name,[obj.name]]
+
+class Sign(Stream, ToStream):
+    """
+        Implement the sign function given an input. 
+
+        :param input: the input for the sign function
+        :type obj: Tensor
+
+        Example:
+            >>> x = Sign(x)
+    """
+    @enforce_types
+    def __init__(self, obj:Stream|Parameter|Constant) -> Stream:
+        obj = toStream(obj)
+        check(type(obj) is Stream, TypeError,
+              f"The type of {obj} is {type(obj)} and is not supported for sign operation.")
+        super().__init__(sign_relation_name+str(Stream.count), obj.json, obj.dim)
+        self.json['Relations'][self.name] = [sign_relation_name,[obj.name]]
 
 class Sum(Stream, ToStream):
     @enforce_types
@@ -274,6 +275,18 @@ def createNeg(self, *inputs):
     #: :noindex:
     return Neg_Layer()
 
+class Sign_Layer(nn.Module):
+    #: :noindex:
+    def __init__(self):
+        super(Sign_Layer, self).__init__()
+
+    def forward(self, x):
+        return torch.sign(x)
+
+def createSign(self, *inputs):
+    #: :noindex:
+    return Sign_Layer()
+
 class Sum_Layer(nn.Module):
     #: :noindex:
     def __init__(self):
@@ -294,6 +307,7 @@ setattr(Model, div_relation_name, createDiv)
 setattr(Model, pow_relation_name, createPow)
 
 setattr(Model, neg_relation_name, createNeg)
+setattr(Model, sign_relation_name, createSign)
 
 setattr(Model, sum_relation_name, createSum)
 
